3d967d6c33768432bc2bde054814a5a7a95b957d
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-07-31 18:58:51 +0530

    tst

---
+++ ES6-Async/syncvsasync/two.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+        console.log("Request - 1")
+}, 10000);
+
+setTimeout(() => {
+    console.log("Request - 2")
+}, 4000);
+
+setTimeout(() => {
+    console.log("Request - 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Request - 4")
+}, 1000);
