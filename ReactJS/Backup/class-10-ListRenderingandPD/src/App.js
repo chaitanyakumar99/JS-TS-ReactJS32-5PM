7160e1b5e8871c15671018757c751d672f428a0d
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-08-19 18:33:38 +0530

    t

---
+++ ReactJS/Backup/class-10-ListRenderingandPD/src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import Employee from "./ListRending/Employee"
+let App = ()=>{
+
+    return <Fragment>
+            <h1>App Component</h1>
+            <hr/>  
+            <Employee/>
+          </Fragment>
+}
+export default App
--- ReactJS/react5/src/App.js
+++ ReactJS/react5/src/App.js
@@ -1,11 +1,14 @@
-import { Fragment } from "react"
-import Employee from "./ListRending/Employee"
-let App = ()=>{
-
-    return <Fragment>
-            <h1>App Component</h1>
-            <hr/>  
-            <Employee/>
-          </Fragment>
+import React from 'react'
+import Products from './EveningBinding/Products'
+import Message from './Message/Message'
+import Users from './Users/Users'
+const App = () => {
+  return <div>
+            <h2>App Component</h2>
+            <hr />
+         {   /* <Products/> */ }
+         <Users/>
+        </div>
 }
+
 export default App
